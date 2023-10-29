b81406227699ab46ba25558a42334e2157efe4f0
clarasanchez <claarisanchez@gmail.com>
2023-10-29 13:50:09 -0300

    mobing image files and updating style

--- src/Components/CardProduct/index.jsx
+++ src/Components/CardProduct/index.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
 import './style.css'
 
-const CardProduct = ({ id, name, quantityToShop, img, type, setModal, setData }) => {
+const CardProduct = ({ id, name, quantityToShop, backgroundImg, img, type, setModal, setData }) => {
 
   const handlerData = () => {
     setModal && setModal(true);
@@ -11,21 +11,18 @@ const CardProduct = ({ id, name, quantityToShop, img, type, setModal, setData })
       type,
     });
   };
-
   return (
-    <>
-      <div style={img ? { backgroundImage: `url(${img}` } : { backgroundColor: '#5a050b' }} className="card-product" onClick={handlerData}>
-        {
-
-          !setModal ?
-            <Link to={`/category/${id}`} className="link-dark link-underline-opacity-0">
-              <p className="card-customized-title">{name}</p>
-            </Link>
-            :
-            <p className="card-customized-title">{name}</p>
-        }
-      </div>
-    </>
+    <div className="card-product" style={backgroundImg && {backgroundImage: `url(${backgroundImg})`}} onClick={handlerData}>
+      {img && <img className="card-image" src={img} />}
+      {
+        !setModal ?
+          <Link to={`/category/${id}`} className="link-dark link-underline-opacity-0">
+            <p className="card-customized-title"  style={backgroundImg && {color: "white", fontWeight: "700"}}>{name}</p>
+          </Link>
+          :
+          <p className="card-customized-title">{name}</p>
+      }
+    </div>
   )
 };
 
--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import { useCartContext } from '../../context/CartContext';
 import ModalCart from '../ModalCart'
 import './style.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
   const { cart } = useCartContext();
@@ -17,14 +19,12 @@ function Navbar() {
       <nav className="navbar navbar-dark custom-navbar row d-flex justify-content-between">
         <div className="col-10">
           <Link to="/" >
-            <img src="/src/assets/logo.png" style={{ height: "50px" }} />
+            <img src="/public/logo.png" style={{ height: "50px" }} />
           </Link>
         </div>
         <div className="cart-icon col-1 mr-3 d-flex justify-content-end" onClick={handlerCart}>
           {cart.length}
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-cart" viewBox="0 0 16 16">
-            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-          </svg>
+          <FontAwesomeIcon icon={faCartShopping} />
         </div>
       </nav>
     </>
